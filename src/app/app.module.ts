3a3c8e4e1aa8a6e293c87066885245da5d06ae3e
Wilfred <wilcr20@gmail.com>
2019-05-15 13:50:41 -0600

    Creación de rutas y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from 'src/components/home/home.component';
+import {ProfileComponent} from 'src/components/profile/profile.component';
+import {DocumentationComponent} from 'src/components/documentation/documentation.component';
+import {AuthComponent} from 'src/components/auth/auth.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    DocumentationComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
